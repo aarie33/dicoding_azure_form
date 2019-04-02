44dec0c758b796a490f1a1e3b229778c8778d5d0
Ahmad Syamsul Arifin <aarie33ahmad@gmail.com>
2019-04-02 20:48:50 +0700

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,6 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:arieserver.database.windows.net,1433; Database = firstsql", "aarie33", "Arie321321");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print("koneksi lancar");
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
