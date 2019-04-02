1b59b60051644de5b8cf2194c2a0ee31f0178263
Ahmad Syamsul Arifin <aarie33ahmad@gmail.com>
2019-04-02 22:13:09 +0700

    Update user.php

--- user.php
+++ user.php
@@ -30,7 +30,7 @@ include('header.php');?>
             		$stmt = $conn->query($sql_select);
             		$users = $stmt->fetchAll();
 			print_r($users);
-            		if(count($users) > 0) { ?>
+            		//if(count($users) > 0) { ?>
 			    <table class="table table-hover table-striped mt-4">
 			    	<caption>Data user</caption>
 			    	<thead>
@@ -61,9 +61,9 @@ include('header.php');?>
 			    	<?php } ?>
 			    	</tbody>
 			    </table>
-			<?php }else {
-                echo "<h3>No one is currently registered.</h3>";
-            } ?>
+			<?php //}else {
+                //echo "<h3>No one is currently registered.</h3>";
+            //} ?>
 		</div>
 	</div>
 </main>
